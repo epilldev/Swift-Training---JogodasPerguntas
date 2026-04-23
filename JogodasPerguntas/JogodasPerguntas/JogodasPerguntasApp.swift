627821c86138a67a7e2b7a215c41ddd8e68ec38f
Felipe J. <35155805+epilldev@users.noreply.github.com>
2026-04-23 13:12:58 -0300

    feat: inicializa projeto iOS em Swift com estrutura base
    - Cria projeto iOS utilizando Swift
    - Configura estrutura inicial do app (App, Views e Assets)
    - Adiciona arquivos padrão gerados pelo Xcode
    - Define base para desenvolvimento do jogo de perguntas

---
+++ JogodasPerguntas/JogodasPerguntas/JogodasPerguntasApp.swift
@@ -0,0 +1,17 @@
+//
+//  JogodasPerguntasApp.swift
+//  JogodasPerguntas
+//
+//  Created by Oliveira, Felipe Joaquim de on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct JogodasPerguntasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
